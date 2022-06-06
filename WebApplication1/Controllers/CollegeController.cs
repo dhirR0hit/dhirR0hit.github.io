5a5d1fced16eae3df303808c0094d65a87265fe9
dhir0hit <rohit786dhir@gmail.com>
2022-06-06 03:27:17 -0400

    Initial commit

---
+++ WebApplication1/Controllers/CollegeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CollegeController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        
+    }
+}
